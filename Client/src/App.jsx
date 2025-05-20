466219df47e6ff2cf7dd5bbd7bd318bf414cf325
yasanmaduranga <yasanmkahangama@gmail.com>
2025-05-20 23:24:09 +0530

    initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">Tailwind CSS v4.1.7 is working!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ Server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const mongoUrl = process.env.MONGOURL;
+
+mongoose.connect(mongoUrl)
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+  app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
